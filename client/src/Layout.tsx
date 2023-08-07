8ce5f5e86622cf45bc050ba7b23aa28d9a0f3ec8
hitendraquessou <sanjay_quessou@yahoo.co.uk>
2023-08-07 23:19:03 +0400

    feat: Implemented Layout.tsx

---
+++ client/src/Layout.tsx
@@ -0,0 +1,28 @@
+import { Route, Routes } from "react-router-dom";
+import { Link } from "react-router-dom";
+import Confessions from "./components/Confessions";
+import Misdemeanour from "./components/misdemeanour";
+import { NotFound } from "./components/Not_Found";
+
+export const Layout = () => {
+  <>
+    <ul>
+      <li>
+        <Link to="/">Home</Link>
+      </li>
+      <li>
+        <Link to="/misdemeanour">Misdemeanour</Link>
+      </li>
+      <li>
+        <Link to="confessions">Confessions</Link>
+      </li>
+    </ul>
+    <Routes>
+      <Route path="/">
+        <Route path="confessions" element={<Confessions />} />
+        <Route path="misdemeanour" element={<Misdemeanour />} />
+        <Route path="*" element={<NotFound />} />
+      </Route>
+    </Routes>
+  </>;
+};
