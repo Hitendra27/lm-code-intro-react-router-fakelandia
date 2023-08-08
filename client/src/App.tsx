9a4dfd6300c0d18798925e83c969e97eede99040
hitendraquessou <sanjay_quessou@yahoo.co.uk>
2023-08-08 22:00:29 +0400

    update: Updated the Layout.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,21 +1,10 @@
-import { useState } from "react";
 import "./App.css";
-import { Route, Routes } from "react-router-dom";
-import Misdemeanour from "./components/misdemeanour";
-import { NotFound } from "./components/Not_Found";
-import Confessions from "./components/Confessions";
+import { Layout } from "./Layout";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <div className="App">
-      <Routes>
-        <Route path="/" />
-        <Route path="confessions" element={<Confessions />} />
-        <Route path="misdemeanour" element={<Misdemeanour />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
+      <Layout />
     </div>
   );
 }
--- client/src/Layout.tsx
+++ client/src/Layout.tsx
@@ -1,28 +1,26 @@
 import { Route, Routes } from "react-router-dom";
 import { Link } from "react-router-dom";
 import Confessions from "./components/Confessions";
-import Misdemeanour from "./components/misdemeanour";
 import { NotFound } from "./components/Not_Found";
+import Misdemeanour from "./components/Misdemeanour";
 
-export const Layout = () => {
-  <>
-    <ul>
-      <li>
-        <Link to="/">Home</Link>
-      </li>
-      <li>
-        <Link to="/misdemeanour">Misdemeanour</Link>
-      </li>
-      <li>
-        <Link to="confessions">Confessions</Link>
-      </li>
-    </ul>
-    <Routes>
-      <Route path="/">
-        <Route path="confessions" element={<Confessions />} />
-        <Route path="misdemeanour" element={<Misdemeanour />} />
-        <Route path="*" element={<NotFound />} />
-      </Route>
-    </Routes>
-  </>;
-};
+export const Layout: React.FC = () => <>
+  <ul>
+    <li>
+      <Link to="/">Home</Link>
+    </li>
+    <li>
+      <Link to="/misdemeanour">Misdemeanour</Link>
+    </li>
+    <li>
+      <Link to="confessions">Confessions</Link>
+    </li>
+  </ul>
+  <Routes>
+    <Route path="/">
+      <Route path="confessions" element={<Confessions />} />
+      <Route path="misdemeanour" element={<Misdemeanour />} />
+      <Route path="*" element={<NotFound />} />
+    </Route>
+  </Routes>
+</>;
