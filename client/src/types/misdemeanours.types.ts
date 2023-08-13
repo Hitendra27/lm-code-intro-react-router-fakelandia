7000d657f678ca55d81e4f6651a61dd72b4b7c85
hitendraquessou <sanjay_quessou@yahoo.co.uk>
2023-08-13 23:44:01 +0400

    feat: Implements types in Client

---
+++ client/src/types/misdemeanours.types.ts
@@ -0,0 +1,16 @@
+export const MISDEMEANOURS = [
+  "rudeness 🤪",
+  "vegetables 🥗",
+  "lift 🗣",
+  "united 😈",
+] as const;
+export type MisdemeanourKind = (typeof MISDEMEANOURS)[number];
+
+export const JUST_TALK = "just-talk";
+export type JustTalk = typeof JUST_TALK;
+
+export type Misdemeanour = {
+  citizenId: number;
+  misdemeanour: MisdemeanourKind;
+  date: string;
+};
